8bcdce99b3d8eb2b7abc5021c4ca7537035cabe3
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-08-12 17:32:52 -0500

    Initial inputs for madlib

--- Madlib.py
+++ Madlib.py
@@ -1,8 +1,15 @@
 # Program that prints out a madlib based on users input
 # string concatenation (how to put strings together)
+noun1 = input("Enter a Noun: ")
+noun2 = input("Enter a Noun: ")
+pluraln = input("Enter a Plural Noun: ")
+adj = input("Enter a Adjective: ")
+verbing = input("Enter a verb ending in -ing: ")
+
+
 madlib1 = f"\nTitle: The Wacky Adventure\
-    \nOnce upon a time in a [noun] far, far away, there lived a [adjective] [noun]. \
-    \nThis [noun] was known for their love of [plural noun] and their peculiar habit of [verb ending in -ing] at the moon. \
+    \nOnce upon a time in a town far, far away, there lived a {adj} {noun1}. \
+    \nThis {noun2} was known for their love of {pluraln} and their peculiar habit of {verbing} at the moon. \
     \nOne fine [day of the week], the [noun] stumbled upon a mysterious [noun], shimmering with [color] lights. \
     \nFilled with [emotion], the [noun] couldn't resist the temptation and decided to [verb] into the unknown. Little did they know, \
     \nthis would be the beginning of an extraordinary journey. As soon as the [noun] stepped inside, they were greeted by a [adjective] \
@@ -13,7 +20,7 @@ madlib1 = f"\nTitle: The Wacky Adventure\
     \nunlock the path forward. The journey was filled with laughter, [adjective] surprises, and countless [noun] to tell. Finally, after facing \
     \nmany challenges, the duo reached the fabled [noun]. As they stood before it, the [noun] couldn't help but feel a mix of anticipation and \
     \n[emotion]. With a deep breath, they touched the artifact and made their wish. What did they wish for, you may ask? That's a secret only \
-    \nthe [noun] and the [noun] know. Satisfied and filled with newfound [emotion], the [noun] and [name] bid farewell to the enchanted [noun] \
-    \nand returned home. They would forever cherish the memories of their wacky adventure, reminding them that sometimes, the greatest stories come from embracing the unknown"
+    \nthe [noun] and the [noun] know. Satisfied and filled with newfound [emotion], the [noun] and [name] bid farewell to the enchanted [noun] and returned home. \
+    \nThey would forever cherish the memories of their wacky adventure, reminding them that sometimes, the greatest stories come from embracing the unknown"
 
 print(madlib1) 
