f79544751310206a037118bd5272e29d9b9a5790
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-08-14 11:56:52 -0500

    Finish all input variables in madlib

--- Madlib.py
+++ Madlib.py
@@ -1,26 +1,43 @@
 # Program that prints out a madlib based on users input
 # string concatenation (how to put strings together)
-adj = input("Enter a Adjective: ")
+adj1 = input("Enter a Adjective: ")
+adj2 = input("Enter a Adjective: ")
+adj3 = input("Enter a Adjective: ")
+adj4 = input("Enter a Adjective: ")
 noun1 = input("Enter a Noun: ")
 noun2 = input("Enter a Noun: ")
-pluraln = input("Enter a Plural Noun: ")
-verbing = input("Enter a verb ending in -ing: ")
+noun3 = input("Enter a Noun: ")
+noun4 = input("Enter a Noun: ")
+noun5 = input("Enter a Noun: ")
+pluralN1 = input("Enter a Plural Noun: ")
+pluralN2 = input("Enter a Plural Noun: ")
+pluralN3 = input("Enter a Plural Noun: ")
+pluralN4 = input("Enter a Plural Noun: ")
+verbing1 = input("Enter a Verb ending in -ing: ")
+verbing2 = input("Enter a Verb ending in -ing: ")
+color = input("Enter a Color: ")
+emotion1 = input("Enter a Emotion: ")
+emotion2 = input("Enter a Emotion: ")
+emotion3 = input("Enter a Emotion: ")
+verb1 = input("Enter a Verb: ")
+name = input("Enter a Name: ")
+mythicalCreature = input("Enter a Mythical Creature: ")
 
 
 madlib1 = f"\nTitle: The Wacky Adventure\
-    \nOnce upon a time in a town far, far away, there lived a {adj} {noun1}. \
-    \nThis {noun1} was known for their love of {pluraln} and their peculiar habit of {verbing} at the moon. \
-    \nOne fine [day of the week], the {noun1} stumbled upon a mysterious {noun2}, shimmering with [color] lights. \
-    \nFilled with [emotion], the [noun] couldn't resist the temptation and decided to [verb] into the unknown. Little did they know, \
-    \nthis would be the beginning of an extraordinary journey. As soon as the [noun] stepped inside, they were greeted by a [adjective] \
-    \n[noun] who introduced themselves as [name]. [name] had an unusual request for the adventurous [noun]. They needed help retrieving the \
-    \nlegendary [noun] of [mythical creature]. It was said to possess the power of [verb ending in -ing] and could grant any [noun] one wish. \
-    \nEager for excitement, the [noun] agreed to embark on the quest. Together, they traversed through [adjective] forests, crossed treacherous \
-    \n[plural noun], and encountered mischievous [plural noun]. Along the way, they collected [plural noun] and solved perplexing riddles to \
-    \nunlock the path forward. The journey was filled with laughter, [adjective] surprises, and countless [noun] to tell. Finally, after facing \
-    \nmany challenges, the duo reached the fabled [noun]. As they stood before it, the [noun] couldn't help but feel a mix of anticipation and \
-    \n[emotion]. With a deep breath, they touched the artifact and made their wish. What did they wish for, you may ask? That's a secret only \
-    \nthe [noun] and the [noun] know. Satisfied and filled with newfound [emotion], the [noun] and [name] bid farewell to the enchanted [noun] and returned home. \
+    \nOnce upon a time in a town far, far away, there lived a {adj1} {noun1}. \
+    \nThis {noun1} was known for their love of {pluralN1} and their peculiar habit of {verbing1} at the moon. \
+    \nOne fine [day of the week], the {noun1} stumbled upon a mysterious {noun2}, shimmering with {color} lights. \
+    \nFilled with {emotion1}, the {noun1} couldn't resist the temptation and decided to {verb1} into the unknown. Little did they know, \
+    \nthis would be the beginning of an extraordinary journey. As soon as the {noun1} stepped inside, they were greeted by a {adj2} \
+    \n{noun3} who introduced themselves as {name}. {name} had an unusual request for the adventurous {noun3}. They needed help retrieving the \
+    \nlegendary {noun4} of {mythicalCreature}. It was said to possess the power of {verbing2} and could grant anyone one wish. \
+    \nEager for excitement, the {noun1} agreed to embark on the quest. Together, they traversed through {adj3} forests, crossed treacherous \
+    \n{pluralN2}, and encountered mischievous {pluralN3}. Along the way, they collected {pluralN4} and solved perplexing riddles to \
+    \nunlock the path forward. The journey was filled with laughter, {adj4} surprises, and countless stories to tell. Finally, after facing \
+    \nmany challenges, the duo reached the fabled {noun5}. As they stood before it, the {noun1} couldn't help but feel a mix of anticipation and \
+    \n{emotion2}. With a deep breath, they touched the artifact and made their wish. What did they wish for, you may ask? That's a secret only \
+    \nthe {noun1} and the {noun3} know. Satisfied and filled with newfound {emotion3}, the {noun1} and {name} bid farewell to the enchanted {noun2} and returned home. \
     \nThey would forever cherish the memories of their wacky adventure, reminding them that sometimes, the greatest stories come from embracing the unknown"
 
 print(madlib1) 
