9c327a678e937dc8d2ccf97c056da468ff0c9d8f
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-08-15 04:12:04 -0500

    Updated madlib

--- Madlib.py
+++ Madlib.py
@@ -22,15 +22,16 @@ emotion3 = input("Enter a Emotion: ")
 verb1 = input("Enter a Verb: ")
 name = input("Enter a Name: ")
 mythicalCreature = input("Enter a Mythical Creature: ")
+weekDay = input("Enter a Week Day: ")
 
 
 madlib1 = f"\nTitle: The Wacky Adventure\
     \nOnce upon a time in a town far, far away, there lived a {adj1} {noun1}. \
     \nThis {noun1} was known for their love of {pluralN1} and their peculiar habit of {verbing1} at the moon. \
-    \nOne fine [day of the week], the {noun1} stumbled upon a mysterious {noun2}, shimmering with {color} lights. \
+    \nOne fine {weekDay}, the {noun1} stumbled upon a mysterious {noun2}, shimmering with {color} lights. \
     \nFilled with {emotion1}, the {noun1} couldn't resist the temptation and decided to {verb1} into the unknown. Little did they know, \
     \nthis would be the beginning of an extraordinary journey. As soon as the {noun1} stepped inside, they were greeted by a {adj2} \
-    \n{noun3} who introduced themselves as {name}. {name} had an unusual request for the adventurous {noun3}. They needed help retrieving the \
+    \n{noun3} who introduced themselves as {name}. {name} had an unusual request for the adventurous {noun1}. They needed help retrieving the \
     \nlegendary {noun4} of {mythicalCreature}. It was said to possess the power of {verbing2} and could grant anyone one wish. \
     \nEager for excitement, the {noun1} agreed to embark on the quest. Together, they traversed through {adj3} forests, crossed treacherous \
     \n{pluralN2}, and encountered mischievous {pluralN3}. Along the way, they collected {pluralN4} and solved perplexing riddles to \
