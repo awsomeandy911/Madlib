ed1ba46bd770e76cd569c73f00a2cf197fb72214
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-08-17 12:41:23 -0500

    Updated 2nd madlib

--- Madlib.py
+++ Madlib.py
@@ -43,13 +43,14 @@ madlib1 = f"\nTitle: The Wacky Adventure\
 
 print(madlib1) 
 
+
 madlib2 = f"\nTitle: The Job Interview\
-Once upon a [day of the week], in a [adjective] city, a [noun] named Bob decided to go for a job interview at the [adjective] company called Silly Solutions. Bob was nervous, but he wore his lucky [color] socks for confidence.\
-When he arrived, the receptionist had a [noun] on her desk that looked like a dancing [animal]. It was [verb ending in -ing] to a silly song, and Bob couldn't help but [verb] along.\
-Finally, it was Bob's turn to meet the [adjective] interviewer, who had a collection of [plural noun] shaped like famous [celebrity] heads. The interviewer asked, ""If you were a [food item], what flavor would you be?""\
-Bob didn't hesitate and said, ""I'd be [adjective] cheese, because I'm sharp and always up for a good [noun]!""\
-The interviewer burst out [verb ending in -ing] and high-fived Bob, saying, ""You're hired! We need more [adjective] people like you around here.""\
-And so, Bob's unusual answer landed him the job at Silly Solutions, where every day felt like a [adjective] adventure filled with [noun] and [verb ending in -ing]."
+     \nOnce upon a {weekDay1}, in a {adj} city, a {noun} named Bob decided to go for a job interview at the {adj} company called Silly Solutions. Bob was nervous, but he wore his lucky {color} socks for confidence.\
+     \nWhen he arrived, the receptionist had a {noun} on her desk that looked like a dancing {animal}. It was {verbing} to a silly song, and Bob couldn't help but {verb} along.\
+     \ninally, it was Bob's turn to meet the {adj} interviewer, who had a collection of {pluralN} shaped like famous {celeb} heads. The interviewer asked, ""If you were a {fooditem}, what flavor would you be?""\
+     \nBob didn't hesitate and said, ""I'd be {adj} cheese, because I'm sharp and always up for a good {noun}!""\
+     \nThe interviewer burst out {verbing} and high-fived Bob, saying, ""You're hired! We need more {adj} people like you around here.""\
+     \nAnd so, Bob's unusual answer landed him the job at Silly Solutions, where every day felt like a {adj} adventure filled with {noun} and {verbing}."
 
 madlib3 = f"\nTitle: The Silent Goodbye\
 In a [adjective] world, a deep [emotion] settled over [noun]. The once [adjective] sky seemed to mourn, casting a [color] pallor on everything.\
