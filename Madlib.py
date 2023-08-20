75171598246f298b8ae1992a44dca60b38c11263
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-08-20 10:38:00 -0500

    2nd madlib variables

--- Madlib.py
+++ Madlib.py
@@ -53,12 +53,12 @@ madlib2 = f"\nTitle: The Job Interview\
     \nAnd so, Bob's unusual answer landed him the job at Silly Solutions, where every day felt like a {adj} adventure filled with {noun} and {verbing}."
 
 madlib3 = f"\nTitle: The Silent Goodbye\
-    \nIn a {adj} world, a deep [emotion] settled over [noun]. The once [adjective] sky seemed to mourn, casting a [color] pallor on everything.\
-    \nIn the midst of [noun], a [noun] faced a heart-wrenching [noun]. The [adjective] loss of a loved one left a void, like an endless [noun].\
-    \nDays turned into [plural noun], each carrying the weight of [emotion]. The [noun] echoed with memories, and [noun] felt like a distant dream.\
-    \nAmidst the [adjective] sadness, there were [noun] of the person who was no longer there. A favorite [noun], a cherished [noun], all remained as bittersweet [noun] of their absence.\
-    \nFriends and family gathered, sharing [adjective] stories through [noun]. Tears fell like [noun], and the ache of [emotion] hung heavy.\
-    \nAs time passed, the pain began to [verb]. But the [noun] remained, a quiet reminder of what was lost. The world moved on, yet the [adjective] sorrow lingered, a testament to a love that would never [verb] fade."
+    \nIn a {adj} world, a deep {emotion} settled over {noun}. The once {adj} sky seemed to mourn, casting a {color} pallor on everything.\
+    \nIn the midst of {noun}, a {noun} faced a heart-wrenching {noun}. The {adjective} loss of a loved one left a void, like an endless {noun}.\
+    \nDays turned into {pluralnoun}, each carrying the weight of {emotion}. The {noun} echoed with memories, and {noun} felt like a distant dream.\
+    \nAmidst the {adj} sadness, there were {noun} of the person who was no longer there. A favorite {noun}, a cherished {noun}, all remained as bittersweet {noun} of their absence.\
+    \nFriends and family gathered, sharing {adj} stories through {noun}. Tears fell like {noun}, and the ache of {emotion} hung heavy.\
+    \nAs time passed, the pain began to {verb}. But the {noun} remained, a quiet reminder of what was lost. The world moved on, yet the {adjective} sorrow lingered, a testament to a love that would never {verb} fade."
 
 madlib4 = f"\nTitle: Enchanted Moments\
     \nIn a world aglow with [color] hues and [adjective] whispers, two [noun] found themselves drawn together. Their eyes met, igniting a spark of [emotion] that danced between them.\
