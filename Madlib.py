f8d187b93f39b2ecc058098ccb3fd8ef02e9636d
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-08-27 02:33:20 -0500

    Random madlib from main file

--- Madlib.py
+++ Madlib.py
@@ -1,2 +1,9 @@
 # Program that prints out a madlib based on users input
 # string concatenation (how to put strings together)
+# used for randomizing madlib
+from madlibs import LoveMadlib, SadMadlib, JobInterview, WackyAdventure
+import random
+
+if __name__ == "__main__":
+    m = random.choice([LoveMadlib, SadMadlib, JobInterview, WackyAdventure])
+    m.madlib()
