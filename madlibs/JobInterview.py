0cdf078ca93d2c250043c0cdfe2b2e1d1bfbbe65
Andy Le <47935760+awsomeandy911@users.noreply.github.com>
2023-08-30 12:21:47 -0500

    Finish rest of madlib

--- madlibs/JobInterview.py
+++ madlibs/JobInterview.py
@@ -1,8 +1,32 @@
 # Job Interview madlib
+weekDay1 = input("Enter a WeekDay: ")
+animal = input("Enter a Animal: ")
+color = input("Enter a Color: ")
+verb = input("Enter a Verb: ")
+celeb = input("Enter the Name of a Celebrity: ")
+food = input("Enter a Food: ")
+adj1 = input("Enter a Adjective: ")
+adj2 = input("Enter a Adjective: ")
+adj3 = input("Enter a Adjective: ")
+adj4 = input("Enter a Adjective: ")
+adj5 = input("Enter a Adjective: ")
+adj6 = input("Enter a Adjective: ")
+noun1 = input("Enter a Noun: ")
+noun2 = input("Enter a Noun: ")
+noun3 = input("Enter a Noun: ")
+noun4 = input("Enter a Noun: ")
+pluralN = input("Enter a Plural Noun: ")
+verbing1 = input("Enter a Verb ending in -ing: ")
+verbing2 = input("Enter a Verb ending in -ing: ")
+verbing3 = input("Enter a Verb ending in -ing: ")
+
+
 madlib = f"\nTitle: The Job Interview\
-    \nOnce upon a {weekDay1}, in a {adj} city, a {noun} named Bob decided to go for a job interview at the {adj} company called Silly Solutions. Bob was nervous, but he wore his lucky {color} socks for confidence.\
-    \nWhen he arrived, the receptionist had a {noun} on her desk that looked like a dancing {animal}. It was {verbing} to a silly song, and Bob couldn't help but {verb} along.\
-    \ninally, it was Bob's turn to meet the {adj} interviewer, who had a collection of {pluralN} shaped like famous {celeb} heads. The interviewer asked, ""If you were a {fooditem}, what flavor would you be?""\
-    \nBob didn't hesitate and said, ""I'd be {adj} cheese, because I'm sharp and always up for a good {noun}!""\
-    \nThe interviewer burst out {verbing} and high-fived Bob, saying, ""You're hired! We need more {adj} people like you around here.""\
-    \nAnd so, Bob's unusual answer landed him the job at Silly Solutions, where every day felt like a {adj} adventure filled with {noun} and {verbing}."
+    \nOnce upon a {weekDay1}, in a {adj1} city, a {noun1} named Bob decided to go for a job interview at the {adj2} company called Silly Solutions. Bob was nervous, but he wore his lucky {color} socks for confidence.\
+    \nWhen he arrived, the receptionist had a {noun2} on her desk that looked like a dancing {animal}. It was {verbing1} to a silly song, and Bob couldn't help but {verb} along.\
+    \ninally, it was Bob's turn to meet the {adj3} interviewer, who had a collection of {pluralN} shaped like famous {celeb} heads. The interviewer asked, \
+    \n\"If you were a {food}, what flavor would you be?\" Bob didn't hesitate and said, \"I'd be {adj4} cheese, because I'm sharp and always up for a good {noun3}!\"\
+    \nThe interviewer burst out {verbing2} and high-fived Bob, saying, \"You're hired! We need more {adj5} people like you around here.\"\
+    \nAnd so, Bob's unusual answer landed him the job at Silly Solutions, where every day felt like a {adj6} adventure filled with {noun4} and {verbing3}."
+
+print(madlib)
--- madlibs/SadMadlib.py
+++ madlibs/SadMadlib.py
@@ -6,23 +6,30 @@ adj4 = input("Enter a Adjective: ")
 adj5 = input("Enter a Adjective: ")
 adj6 = input("Enter a Adjective: ")
 noun1 = input("Enter a Noun: ")
-noun1 = input("Enter a Noun: ")
-noun1 = input("Enter a Noun: ")
-noun1 = input("Enter a Noun: ")
-noun1 = input("Enter a Noun: ")
-noun1 = input("Enter a Noun: ")
-noun1 = input("Enter a Noun: ")
-noun1 = input("Enter a Noun: ")
-noun1 = input("Enter a Noun: ")
-noun1 = input("Enter a Noun: ")
-noun1 = input("Enter a Noun: ")
-noun1 = input("Enter a Noun: ")
-noun1 = input("Enter a Noun: ")
+noun2 = input("Enter a Noun: ")
+noun3 = input("Enter a Noun: ")
+noun4 = input("Enter a Noun: ")
+noun5 = input("Enter a Noun: ")
+noun6 = input("Enter a Noun: ")
+noun7 = input("Enter a Noun: ")
+noun8 = input("Enter a Noun: ")
+noun9 = input("Enter a Noun: ")
+noun10 = input("Enter a Noun: ")
+noun11 = input("Enter a Noun: ")
+emotion1 = input("Enter a Emotion: ")
+emotion2 = input("Enter a Emotion: ")
+emotion3 = input("Enter a Emotion: ")
+pluralN = input("Enter a Plural Noun: ")
+color = input("Enter a Color: ")
+verb = input("Enter a Verb: ")
+verb1 = input("Enter a Verb: ")
 
 madlib = f"\nTitle: The Silent Goodbye\
     \nIn a {adj1} world, a deep {emotion1} settled over the {noun1}. The once {adj2} sky seemed to mourn, casting a {color} pallor on everything.\
     \nIn the midst of a {noun2}, a {noun3} faced a heart-wrenching fable. The {adj3} loss of a loved one left a void, like an endless {noun5}.\
-    \nDays turned into {pluralnoun}, each carrying the weight of {emotion2}. The {noun3} echoed with memories, and {noun7} felt like a distant dream.\
-    \nAmidst the {adj4} sadness, there were {noun8} of the person who was no longer there. A favorite {noun9}, a cherished {noun10}, all remained as bittersweet {noun11} of their absence.\
-    \nFriends and family gathered, sharing {adj5} stories through {noun12}. Tears fell like {noun13}, and the ache of {emotion3} hung heavy.\
-    \nAs time passed, the pain began to {verb}. But the {noun14} remained, a quiet reminder of what was lost. The world moved on, yet the {adj6} sorrow lingered, a testament to a love that would never {verb} fade."
+    \nDays turned into {pluralN}, each carrying the weight of {emotion2}. The {noun3} echoed with memories, and {noun2} felt like a distant dream.\
+    \nAmidst the {adj4} sadness, there were {noun6} of the person who was no longer there. A favorite {noun7}, a cherished {noun8}, all remained as bittersweet {noun9} of their absence.\
+    \nFriends and family gathered, sharing {adj5} stories through conversations. Tears fell like {noun10}, and the ache of {emotion3} hung heavy.\
+    \nAs time passed, the pain began to {verb}. But the {noun11} remained, a quiet reminder of what was lost. The world moved on, yet the {adj6} sorrow lingered, a testament to a love that would never {verb1} fade."
+
+print(madlib)
